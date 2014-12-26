43b8148c9b9f70e07fca14ae36886e9e3b7ad078
Leonardo Wistuba de França <leonardowistuba@gmail.com>
2014-12-26 13:12:11 -0200

    **Combate** - Implementa modelo de animação do player ao atacar

--- app/Evil Cave/Nodes/LWFPlayer.m
+++ app/Evil Cave/Nodes/LWFPlayer.m
@@ -45,4 +45,43 @@
     [self.map movementRequestIsInvalid];
 }
 
+- (void)willAttackTile:(LWFTile *)tile withAttack:(LWFAttack *)attack completion:(void (^)(void))someBlock {
+    [super willAttackTile:tile withAttack:attack completion:nil];
+    
+    [self startAttackingAnimation];
+    
+    [someBlock invoke];
+}
+
+- (void)startAttackingAnimation {
+    NSArray *attackingFramesAnimation = [self getAttackingFramesAnimation];
+    
+    [self removeActionForKey:@"attacking_action"];
+    
+    if (attackingFramesAnimation != nil && attackingFramesAnimation.count > 0) {
+        SKAction *animate = [SKAction animateWithTextures:attackingFramesAnimation timePerFrame:0.05f];
+        SKAction *action = [SKAction repeatAction:animate count:2];
+        
+        [self runAction:action];
+    }
+}
+
+- (NSArray *)getAttackingFramesAnimation {
+
+    NSMutableArray *attackingAtlasArray = [NSMutableArray array];
+    NSString *attackingAtlasName = [NSString stringWithFormat:@"%@_attacking_%@", self.spriteImageName, self.currentFacingDirection];
+    SKTextureAtlas *attackingAtlas = [SKTextureAtlas atlasNamed:attackingAtlasName];
+    
+    NSUInteger numImages = attackingAtlas.textureNames.count;
+    for (int i=1; i <= numImages; i++) {
+        NSString *textureName = [NSString stringWithFormat:@"%@_%d", attackingAtlasName, i];
+        SKTexture *texture = [attackingAtlas textureNamed:textureName];
+        texture.filteringMode = SKTextureFilteringNearest;
+        [attackingAtlasArray addObject:texture];
+    }
+    
+    return attackingAtlasArray;
+
+}
+
 @end
