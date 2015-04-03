6c1ebb0baaf5cf566773fc5de4990d71560409af
Leonardo Wistuba de França <leonardowistuba@gmail.com>
2015-04-02 23:25:13 -0300

    [Ataques de longe] - Implementa comportamento em caso de criatura longe do player

--- app/Evil Cave/Utils/Creatures/LWFPoopThrowerRat.m
+++ app/Evil Cave/Utils/Creatures/LWFPoopThrowerRat.m
@@ -10,6 +10,8 @@
 #import "LWFPoopThrowAttack.h"
 #import "LWFPlayer.h"
 #import "LWFMap.h"
+#import "LWFCreature.h"
+#import "LWFTileMap.h"
 
 @implementation LWFPoopThrowerRat
 
@@ -37,9 +39,48 @@
     if ([poopThrowAttack isCreature:self.player inRangeOfTile:self.currentTile]) {
         [self requestAttackToTile:self.player.currentTile withAttack:poopThrowAttack];
     } else {
+        if ([self.player isSurrounded]) {
+            [self finishTurn];
+            return;
+        }
+        
+        LWFTile *closestTile = [self closestNeighborToPlayer];
+        if (closestTile != nil) {
+            [self buildPathToTile:closestTile];
+            
+            if (self.tilePath == nil || self.tilePath.count == 0) {
+                [self finishTurn];
+                return;
+            } else {
+                [self walkToExistingPath];
+            }
+            return;
+        } else {
+            [self finishTurn];
+            return;
+        }
+        
+        
         [self finishTurn];
         return;
     }
+}
+
+- (LWFTile *)closestNeighborToPlayer {
+    LWFTile *originTile = self.currentTile;
+    LWFTile *destinyTile = self.player.currentTile;
+    
+    LWFTile *closest = [self.map.tileMap closestNeighborFromTile:originTile toTile:destinyTile];
+    
+    return closest;
+}
+
+- (BOOL)shouldFollowPlayer {
+    NSUInteger distanceToPlayer = [self.currentTile distanceToTile:self.player.currentTile];
+    if (distanceToPlayer < 5) {
+        return YES;
+    }
+    return NO;
 }
 
 - (NSArray *)getWalkingFramesAnimation {
@@ -55,6 +96,12 @@
     
     NSInteger moveOffsetX = tile.position.x - self.position.x;
     NSInteger moveOffsetY = tile.position.y - self.position.y;
+    
+    if (moveOffsetX < 0) {
+        self.currentFacingDirection = @"left";
+    } else if (moveOffsetX > 0) {
+        self.currentFacingDirection = @"right";
+    }
     
     SKSpriteNode *poop = [SKSpriteNode spriteNodeWithImageNamed:@"poop.jpg"];
     [poop setSize:CGSizeMake(TILE_SIZE, TILE_SIZE)];
