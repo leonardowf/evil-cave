18fb406fcbfe790a28e78d48279533a43890a189
Leonardo Wistuba <leonardowistuba@gmail.com>
2015-07-17 22:25:57 -0300

    [Game Over] - Cria objeto objeto que dispara os eventos de game over

---
+++ app/Evil Cave/Game Over/LWFGameOver.h
@@ -0,0 +1,16 @@
+//
+//  LWFGameOver.h
+//  Evil Cave
+//
+//  Created by Leonardo on 7/17/15.
+//  Copyright (c) 2015 leonardowistuba. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LWFGameOver : NSObject
+
++ (id)sharedGameOver;
+- (void)start;
+
+@end
---
+++ app/Evil Cave/Game Over/LWFGameOver.m
@@ -0,0 +1,51 @@
+//
+//  LWFGameOver.m
+//  Evil Cave
+//
+//  Created by Leonardo on 7/17/15.
+//  Copyright (c) 2015 leonardowistuba. All rights reserved.
+//
+
+#import "LWFGameOver.h"
+
+@implementation LWFGameOver
+
+SINGLETON_FOR_CLASS(GameOver)
+
+- (void)start {
+    
+}
+
+- (void)turnGameBlackAndWhite {
+    
+}
+
+- (void)addGameOverTitle {
+    
+}
+
+- (void)displayAd {
+    
+}
+
+- (void)displayActions {
+    
+}
+
+- (void)requestRestartGame {
+    
+}
+
+#pragma mark - Actions
+- (void)didClickRetry {
+    
+}
+
+- (void)didClickSkillTree {
+    
+}
+
+- (void)didClickMainMenu {
+    
+}
+@end
