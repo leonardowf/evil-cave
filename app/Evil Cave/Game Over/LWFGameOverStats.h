33ecdd6276179c584a7f59042ae720d0193462db
Leonardo Wistuba de França <leonardowistuba@gmail.com>
2015-07-21 22:38:02 -0300

    [Assets] - Opening Chest

---
+++ app/Evil Cave/Game Over/LWFGameOverStats.h
@@ -0,0 +1,17 @@
+//
+//  LWFGameOverStats.h
+//  Evil Cave
+//
+//  Created by Leonardo Wistuba de França on 7/21/15.
+//  Copyright (c) 2015 leonardowistuba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LWFGameOverStats : UIView
+
+@property (strong, nonatomic) IBOutlet UIView *view;
+@property (weak, nonatomic) IBOutlet UIView *containerView;
+@property (weak, nonatomic) IBOutlet UILabel *labelGold;
+
+@end
