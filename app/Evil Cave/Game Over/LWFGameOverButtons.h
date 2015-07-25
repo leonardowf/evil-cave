6ce1c85b19501998ee9642f61efc3e6cf040d9ff
Leonardo Wistuba de França <leonardowistuba@gmail.com>
2015-07-24 22:01:44 -0300

    [Game Over] - Cria view que conterá os botões de gameover

---
+++ app/Evil Cave/Game Over/LWFGameOverButtons.h
@@ -0,0 +1,13 @@
+//
+//  LWFGameOverButtons.h
+//  Evil Cave
+//
+//  Created by Leonardo Wistuba de França on 7/24/15.
+//  Copyright (c) 2015 leonardowistuba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LWFGameOverButtons : UIView
+
+@end
---
+++ app/Evil Cave/Game Over/LWFGameOverButtons.m
@@ -0,0 +1,21 @@
+//
+//  LWFGameOverButtons.m
+//  Evil Cave
+//
+//  Created by Leonardo Wistuba de França on 7/24/15.
+//  Copyright (c) 2015 leonardowistuba. All rights reserved.
+//
+
+#import "LWFGameOverButtons.h"
+
+@implementation LWFGameOverButtons
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
