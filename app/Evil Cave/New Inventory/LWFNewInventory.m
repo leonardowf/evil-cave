9fdd9641138b67ad32406aeea525d42bd4ae2930
Leonardo Wistuba de França <leonardowistuba@gmail.com>
2015-08-22 22:27:01 -0300

    [Inventory] - Começa implementação de novo inventário

---
+++ app/Evil Cave/New Inventory/LWFNewInventory.m
@@ -0,0 +1,13 @@
+//
+//  LWFNewInventory.m
+//  Evil Cave
+//
+//  Created by Leonardo Wistuba de França on 8/22/15.
+//  Copyright (c) 2015 leonardowistuba. All rights reserved.
+//
+
+#import "LWFNewInventory.h"
+
+@implementation LWFNewInventory
+
+@end
