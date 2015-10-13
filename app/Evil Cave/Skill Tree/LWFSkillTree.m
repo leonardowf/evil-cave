24dc464d3257ca715602a03d987e472d068fe794
Leonardo Wistuba de França <leonardowistuba@gmail.com>
2015-10-12 21:17:38 -0300

    Começa implementação de Skill Tree

---
+++ app/Evil Cave/Skill Tree/LWFSkillTree.m
@@ -0,0 +1,15 @@
+//
+//  LWFSkillTree.m
+//  Evil Cave
+//
+//  Created by Leonardo Wistuba de França on 10/12/15.
+//  Copyright © 2015 leonardowistuba. All rights reserved.
+//
+
+#import "LWFSkillTree.h"
+
+@implementation LWFSkillTree
+
+SINGLETON_FOR_CLASS(SkillTree)
+
+@end
