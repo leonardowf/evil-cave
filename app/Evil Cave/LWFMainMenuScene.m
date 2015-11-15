4a467eeb635bb62ccbe331bac558efbd9e68e437
Leonardo Wistuba de França <leonardowistuba@gmail.com>
2015-11-14 22:46:35 -0200

    Adiciona botões do menu principal

--- app/Evil Cave/LWFMainMenuScene.m
+++ app/Evil Cave/LWFMainMenuScene.m
@@ -19,12 +19,41 @@
         node.size = size;
         node.position = CGPointZero;
         
+        SKSpriteNode *newGameButton = [self newGameButton];
+        SKSpriteNode *continueButton = [self continueButton];
+        SKSpriteNode *logo = [self logo];
+        
         self.anchorPoint = CGPointMake(0.5, 0.5);
         
         [self addChild:node];
+        [self addChild:newGameButton];
+        [self addChild:continueButton];
+        [self addChild:logo];
+        
+        logo.size = CGSizeMake(200, 200);
+        
+        logo.position = CGPointMake(0, 135);
+        newGameButton.position = CGPointMake(newGameButton.position.x, newGameButton.position.y - 20);
+        
+        continueButton.position = CGPointMake(continueButton.position.x, newGameButton.position.y - newGameButton.size.height - 20);
         
     }
     return self;
+}
+
+- (SKSpriteNode *)newGameButton {
+    SKSpriteNode *newButtonNode = [[SKSpriteNode alloc]initWithImageNamed:@"btn-new-game"];
+    return newButtonNode;
+}
+
+- (SKSpriteNode *)continueButton {
+    SKSpriteNode *continueButtonNode = [[SKSpriteNode alloc]initWithImageNamed:@"btn-continue"];
+    return continueButtonNode;
+}
+
+- (SKSpriteNode *)logo {
+    SKSpriteNode *logoNode = [[SKSpriteNode alloc]initWithImageNamed:@"logo_menu"];
+    return logoNode;
 }
 
 @end
