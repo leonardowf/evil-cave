fbbb18ec2818994f1b34335cccc59fb1e05795b9
Leonardo Wistuba de França <leonardowistuba@gmail.com>
2015-11-25 22:44:00 -0200

    Começa implementação de settings view

---
+++ app/Evil Cave/Custom Views/LWFSettingsView.h
@@ -0,0 +1,13 @@
+//
+//  LWFSettingsView.h
+//  Evil Cave
+//
+//  Created by Leonardo Wistuba de França on 11/25/15.
+//  Copyright © 2015 leonardowistuba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LWFSettingsView : LWFView
+
+@end
---
+++ app/Evil Cave/Custom Views/LWFSettingsView.m
@@ -0,0 +1,13 @@
+//
+//  LWFSettingsView.m
+//  Evil Cave
+//
+//  Created by Leonardo Wistuba de França on 11/25/15.
+//  Copyright © 2015 leonardowistuba. All rights reserved.
+//
+
+#import "LWFSettingsView.h"
+
+@implementation LWFSettingsView
+
+@end
